fea13bdbbb3b9d77cc77c028cf3d9d6bd959d345
Karina <liraoharak@gmail.com>
2024-06-27 17:17:21 -0300

    segundo commit

--- wp-content/uploads/mailchimp-for-wp/debug-log.php
+++ wp-content/uploads/mailchimp-for-wp/debug-log.php
@@ -4,3 +4,5 @@
 [2024-06-23 00:47:11] WARNING: Form 18 > lira******@gm***.com is already subscribed to the selected list(s)
 [2024-06-23 00:47:21] WARNING: Form 18 > lana******@gm***.com is already subscribed to the selected list(s)
 [2024-06-24 13:38:41] WARNING: Form 18 > lana******@gm***.com is already subscribed to the selected list(s)
+[2024-06-26 16:31:40] WARNING: Form 18 > leon*********@gm***.com is already subscribed to the selected list(s)
+[2024-06-26 16:58:22] WARNING: Form 18 > lira******@gm***.com is already subscribed to the selected list(s)
