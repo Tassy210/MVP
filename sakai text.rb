c5fd08dcb7df5feea60a99dae939f2cfd32b9b5d
Kaah Ohara <liraoharak@gmail.com>
2024-07-30 17:18:28 -0300

    menu

---
+++ sakai text.rb
@@ -0,0 +1,53 @@
+Sakai - Longsight 
+Possui fóruns de discusão, onde pode:
+adicionar titulo
+descrição resumida
+descrição
+anexos
+selecionar o tipo de publicação do Tópico
+disponibilidade
+notificação
+tem a opção de marcar todas as mensagem como lidas 
+alterar as permissões de acesso e alterações
+
+Enquetes, onde é possivel:
+adicionar imagens e Dados 
+ver os candidados
+ver as repostas deles
+o tempo de meeting
+o material de recrutamento
+
+Site Info
+
+Estatisticas
+Do Site: 
+é posssivel ver as visitas ao site
+quem visitou
+os membros
+os membros que visitaram 
+os que não e o tempo que ficou
+Atividades é possivel:
+ver a quantidade de eventos por atividade
+o usuario mais ativo
+o relatorio 
+Repositorio, é possivel:
+ver a quantidade de arquivos
+quantos arquivos foram abertos
+a pagina mais aberta
+quem mais abriu arquivos 
+Rubrics, acredito que seja um meio de nota.
+Cronograma-É possivel:
+inserir reuniões
+eventos
+
+Atividades e exercicios: Para os alunos
+Gradebook: É as notas
+Avisos-São avisos que tem
+Assunto
+Salvo por quem
+Datas de criação, inicio e final
+site 
+
+É possível programar as atividades
+É possível gerar certificados a partir de plugins
+
