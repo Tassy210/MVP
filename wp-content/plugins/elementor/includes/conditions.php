a26928ae5213469155764731f3bd23e34ca98235
Leon Tassinari <leontassinari@gmail.com>
2024-08-06 14:57:56 -0300

    Atualização 2

--- wp-content/plugins/elementor/includes/conditions.php
+++ wp-content/plugins/elementor/includes/conditions.php
@@ -84,6 +84,12 @@ class Conditions {
 			} else {
 				preg_match( '/(\w+)(?:\[(\w+)])?/', $term['name'], $parsed_name );
 
+				if (isset($array['background_color'])) {
+					$background_color = $array['background_color'];
+				} else {
+					$background_color = 'default_value'; // or handle it appropriately
+				}
+
 				$value = $comparison[ $parsed_name[1] ];
 
 				if ( ! empty( $parsed_name[2] ) ) {
