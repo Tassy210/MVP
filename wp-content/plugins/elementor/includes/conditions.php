5288465a38c15233d964b024e5212ca7956b2db9
Leon Tassinari <leontassinari@gmail.com>
2024-08-06 15:53:48 -0300

    Espaçamento das seções

--- wp-content/plugins/elementor/includes/conditions.php
+++ wp-content/plugins/elementor/includes/conditions.php
@@ -84,12 +84,6 @@ class Conditions {
 			} else {
 				preg_match( '/(\w+)(?:\[(\w+)])?/', $term['name'], $parsed_name );
 
-				if (isset($array['background_color'])) {
-					$background_color = $array['background_color'];
-				} else {
-					$background_color = 'default_value'; // or handle it appropriately
-				}
-
 				$value = $comparison[ $parsed_name[1] ];
 
 				if ( ! empty( $parsed_name[2] ) ) {
